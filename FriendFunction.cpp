192aae7aba903be5a2c09681f1125ea087fb827e
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2024-08-11 17:47:38 +0530

    Create FriendFunction.cpp

---
+++ FriendFunction.cpp
@@ -0,0 +1,46 @@
+/*
+Friend Function - Like friend class , A friend function has special access to the 
+private and protected members of the class , A friend function can the function of the same class and it can be the member function of another class it has
+some priorty that it can manipulate the private and protected values.
+*/
+
+// C++ program to create a global function as a friend
+// function of some class
+#include <iostream>
+using namespace std;
+
+class base {
+private:
+    int private_variable;
+
+protected:
+    int protected_variable;
+
+public:
+    base()
+    {
+        private_variable = 10;
+        protected_variable = 99;
+    }
+    
+      // friend function declaration
+    friend void friendFunction(base& obj);
+};
+
+
+// friend function definition
+void friendFunction(base& obj)
+{
+    cout << "Private Variable: " << obj.private_variable
+         << endl;
+    cout << "Protected Variable: " << obj.protected_variable;
+}
+
+// driver code
+int main()
+{
+    base object1;
+    friendFunction(object1);
+
+    return 0;
+}
